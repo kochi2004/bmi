e91e9c8e731e37331f2067484d23264dbf8efec1
chou <kochi.chou2004@gmail.com>
2025-04-28 07:38:12 +0800

    first

---
+++ app/src/main/java/com/example/myapplication/tt.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+class tt {
+}
+fun main(){
+    println("Hello Kochi!")
+    val age= 20
+    println(age.inc())
+
+    val name= "Kochi"
+    println(name)
+
+}
+
